65775b61ce3fd62e00adf4f0668cf51238bdd3f4
Florian Stallmann <florian@stallmann.me>
2025-09-02 17:35:20 +0200

    npm update, related changes and gitignore

--- src/utils/shikiAdapter.ts
+++ src/utils/shikiAdapter.ts
@@ -2,6 +2,10 @@ import type { HighlighterGeneric } from "shiki"
 import { createShikiAdapter } from "@chakra-ui/react"
 
 export const shikiAdapter = createShikiAdapter<HighlighterGeneric<any, any>>({
+  theme: {
+    light: 'github-light',
+    dark: 'github-dark'
+  },
   async load() {
     const { createHighlighter } = await import("shiki")
     return createHighlighter({
